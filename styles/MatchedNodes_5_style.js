5cad4b126c4a3f2b7affe6ef0b0a99450db5b8b9
Robert Emerencia <robert.emerencia@gmail.com>
2025-11-13 23:11:46 +0100

    Modify layer order for examples

---
+++ styles/MatchedNodes_5_style.js
@@ -0,0 +1,32 @@
+var size = 0;
+var placement = 'point';
+
+var style_MatchedNodes_5 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "13.0px \'Segoe UI\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("node") !== null) {
+        labelText = String(feature.get("node"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 12.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(51,167,170,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(255,255,255,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
